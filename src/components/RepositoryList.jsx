d1282e85c14055f121b726f9e0fe9cdd9d55bdf2
Saara Satokangas <saarasat@gmail.com>
2021-02-22 09:16:52 +0200

    Exercise 10.23

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,17 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FlatList, View, StyleSheet } from 'react-native';
+import RNPickerSelect from 'react-native-picker-select';
 
 import RepositoryItem from './RepositoryItem';
 import useRepositories from '../hooks/useRepositories';
 import Text from './Text';
+import theme from '../theme';
 
 const styles = StyleSheet.create({
   separator: {
     height: 10,
   },
+  picker: {
+    height: 60,
+    backgroundColor: theme.colors.backgroundPrimary,
+    padding: 15,
+    borderColor: theme.colors.backgroundPrimary,
+    fontFamily: theme.fonts.main
+  }
 });
 
 const ItemSeparator = () => <View style={styles.separator} />;
+
 
 export const RepositoryListContainer = ({ repositories }) => {
   const repositoryNodes = repositories
@@ -31,12 +41,29 @@ export const RepositoryListContainer = ({ repositories }) => {
 };
 
 const RepositoryList = () => {
-  const { data, loading, error } = useRepositories();
+  const [order, setOrder] = useState('latest')
+  const { data, loading, error } = useRepositories(order);
 
   if (loading || !data) return <Text>Loading</Text>;
   if (error) return <Text>Error loading repositories</Text>;
 
-  return <RepositoryListContainer repositories={data.repositories} />;
+  return (
+    <>
+      <RNPickerSelect
+        pickerProps={{
+          style: styles.picker
+        }}
+        value={order}
+        onValueChange={(value) => setOrder(value)}
+        items={[
+            { label: 'Latest repositories', value: 'latest' },
+            { label: 'Highest rated repositories', value: 'highest' },
+            { label: 'Lowest rated repositories', value: 'lowest' },
+        ]}
+      />
+      <RepositoryListContainer repositories={data.repositories} />
+    </>
+  );
 };
 
 export default RepositoryList;
--- src/hooks/useRepositories.js
+++ src/hooks/useRepositories.js
@@ -2,10 +2,31 @@ import { useQuery } from '@apollo/react-hooks';
 
 import { GET_REPOSITORIES } from '../graphql/queries';
 
-const useRepositories = () => {
+const orders = {
+  CREATED_AT: "CREATED_AT",
+  RATING_AVERAGE: "RATING_AVERAGE",
+};
+
+const direction = {
+  ASC: "ASC",
+  DESC: "DESC"
+};
+
+const orderMap = {
+  latest: { orderBy: orders.CREATED_AT, orderDirection: direction.ASC },
+  lowest: { orderBy: orders.RATING_AVERAGE, orderDirection: direction.ASC },
+  highest: { orderBy: orders.RATING_AVERAGE, orderDirection: direction.DESC },
+};
+
+const useRepositories = (order) => {
   const { data, loading, error } = useQuery(GET_REPOSITORIES, {
+    variables: {
+      orderBy: orderMap[order].orderBy,
+      orderDirection: orderMap[order].orderDirection, 
+    },
     fetchPolicy: 'cache-and-network'
   });
+
   return { data, loading, error };
 };
 
